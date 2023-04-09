70d59ea8170a54120bec3ea62767a3cbc8388b99
Kalil Aziz Santos Chami <kalilazizsantoschami@hotmail.com>
2023-04-09 11:05:06 -0300

    test register e2e

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,11 @@
 import { prisma } from '@/lib/prisma'
+import 'dotenv/config'
 import { execSync } from 'node:child_process'
 import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
 
-const generateDatabaseUrlschema = (schema: string) => {
-  if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL is not defined')
-  }
+function generateDatabaseUrlschema(schema: string) {
+  if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not defined')
 
   const url = new URL(process.env.DATABASE_URL)
 
@@ -18,29 +17,19 @@ const generateDatabaseUrlschema = (schema: string) => {
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    // Antes de cada test
-    console.log('setup')
-
-    // Iremos gerar um nome aleatório para o schema
     const schema = randomUUID()
-    const databaseURL = generateDatabaseUrlschema(schema)
+    const databaseUrl = generateDatabaseUrlschema(schema)
 
-    // Iremos sobreescrever a URL de conexão do Prisma
-    process.env.DATABASE_URL = databaseURL
+    process.env.DATABASE_URL = databaseUrl
 
-    // Iremos importar as migrations ao banco de dados
     execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        // Depois de cada test
-        // console.log('teardown')
-
         await prisma.$executeRawUnsafe(
-          `DROP SCHEMA IF EXISTS "${schema}" CASCADE;`
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
         )
 
-        // Desconectamos do banco de dados
         await prisma.$disconnect()
       },
     }
--- src/http/controller/users/register.spec.ts
+++ src/http/controller/users/register.spec.ts
@@ -1,7 +1,26 @@
-import { describe, it, expect } from 'vitest'
+// path: src/http/controller/users/register.spec.ts
+
+import Request from 'supertest'
+import { app } from '@/app'
+
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 describe('Register (e2e)', () => {
-  it('should register a new user', async () => {
-    expect(1).toBe(1)
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await Request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johnDoe@exemple.com',
+      password: '123456',
+    })
+
+    expect(response.status).toEqual(201)
   })
 })
